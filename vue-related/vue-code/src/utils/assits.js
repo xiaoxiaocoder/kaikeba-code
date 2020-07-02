7d347ba12a1a4fd6bb03056c785677e1f38f5edb
xiaoxiaocoder <xiaoxiaocoder@126.com>
2020-07-02 19:00:20 +0800

    vue-code: 完善assits code

--- vue-related/vue-code/src/utils/assits.js
+++ vue-related/vue-code/src/utils/assits.js
@@ -11,6 +11,52 @@ function findComponentsDownward(context, compnentName){
   }, [])
 }
 
+function findComponentDownward(context, compnentName) {
+  let component = null
+  let len = context.$children
+  for (let i = 0; i < len; i++) {
+    const child = context.$children[i];
+    if (child.$options.name === compnentName) {
+      component = child
+    } else {
+      component = findComponentDownward(child, compnentName)
+    }
+    if (component) {
+      break
+    } 
+  }
+  return component
+}
+
+function findComponentUpWard(context, compnentName) {
+  let component = null
+  for (let i = 0; i < context.$parent.length; i++) {
+    const parent = context.$parent[i];
+    if (parent && parent.$options.name === compnentName) {
+      component = parent
+    } else {
+      component = findComponentUpWard(parent, compnentName)
+    }
+    if(component) break
+  }
+  return component
+}
+
+function findComponentsUpWard(context, compnentName) {
+  let components = []
+  context.$parent.forEach(parent => {
+    if (parent.$options.name === compnentName) components = components.concat(parent)
+    const comps = findComponentsUpWard(parent, compnentName)
+    if (comps.length) {
+      components.concat(comps)
+    }
+  })
+  return components
+}
+
 export {
-  findComponentsDownward
+  findComponentsDownward,
+  findComponentDownward,
+  findComponentUpWard,
+  findComponentsUpWard
 }
