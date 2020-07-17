2bd6c73e8edbf0f2755c261a25a2a095f9d14ff2
xiaoxiaocoder <xiaoxiaocoder@126.com>
2020-07-17 19:24:43 +0800

    typescript

---
+++ vue-related/ts/src/shims-vue.d.ts
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import 'vue-router/types/index'
+import 'vuex/types/index'
+import { AxiosInstance } from "axios";
+
+declare module "*.vue" {
+  export default Vue;
+}
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $http: AxiosInstance;
+    $message: Function
+  }
+}
