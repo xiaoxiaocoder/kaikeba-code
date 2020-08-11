60e7ea12ec987dc8f16d752cb1efda0074a51818
xiaoxiaocoder <xiaoxiaocoder@126.com>
2020-08-11 17:23:05 +0800

    react-dom重构

--- react-related/source-code/src/App.js
+++ react-related/source-code/src/App.js
@@ -1,6 +1,29 @@
 // import React from 'react';
 import React from './k-react'
 // import ClassComponent from './components/ClassComponent'
+import './app.css'
+
+class ClassComponent extends React.Component {
+  static defaultProps = {
+    msg: '默认父组件传来的值'
+  }
+  render() {
+    return (
+      <div className={['box', this.props.theme].join(' ')}>
+        {this.props.name} Component
+        <p>{this.props.msg}</p>
+      </div>
+    );
+  }
+}
+
+function FunctionComponent(props) {
+  return (
+    <div>
+      {props.name} Component
+    </div>
+  )
+}
 
 export default function () {
   return (
@@ -8,38 +31,24 @@ export default function () {
       <h1>hello</h1>
       world
       <>123</>
-      <ClassComponent name="Class" />
+      <ClassComponent name="Class" theme="pink" />
       <FunctionComponent name="Functional"/>
       <ul>
-        {[1,2,3].map(item => <li>{item}</li>) }
+        {[1,2,3].map(item => <li key={item}>{item}</li>) }
       </ul>
     </div>
   )
 }
 
-
-class ClassComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.name} Component
-        <h2>{this.props.msg}</h2>
-      </div>
-    );
-  }
-}
-
-ClassComponent.defaultProps = {
-  msg: '默认父组件传来的值'
-}
-
-
-
-function FunctionComponent(props) {
-  console.log('FunctionComponents :>> ', props);
-  return (
-    <div>
-      {props.name} Component
-    </div>
-  )
-}
+// export default (
+//   <div>
+//   <h1>hello</h1>
+//   world
+//   <>123</>
+//   <ClassComponent name="Class" theme="pink" />
+//   <FunctionComponent name="Functional"/>
+//   <ul>
+//     {[1,2,3].map(item => <li>{item}</li>) }
+//   </ul>
+// </div>
+// )
