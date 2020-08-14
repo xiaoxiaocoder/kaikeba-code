b808252637cdd499d6fd1faf56d2ff2e10b96de0
xiaoxiaocoder <xiaoxiaocoder@126.com>
2020-08-15 01:11:19 +0800

    cpoy kkb code

--- react-related/source-code/src/k-react-dom/src/ReactDOM.js
+++ react-related/source-code/src/k-react-dom/src/ReactDOM.js
@@ -1,6 +1,7 @@
 
 // import { render } from './ReactDOMLegacy'
-import { render } from './ReactDOMLegacy-fiber'
+// import { render } from './ReactDOMLegacy-fiber'
+import { render } from './ReactDOMLegacy-fiber-kkb'
 
 export {
   render
